4ae0e9391cb233b068592dc5f0e5c9d2b97c1e65
jaiderbernal90 <jaiderbernal90@gmail.com>
2022-05-17 19:52:22 -0500

    Settins files front

---
+++ Front/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.sass']
+})
+export class AppComponent {
+  title = 'tangelo';
+
+  sd() {
+    console.log(this.title);  
+  }
+}
